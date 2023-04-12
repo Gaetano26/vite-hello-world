576fce1b6021a22a079a1fd03de809a1f0605906
Gaetano26 <gaetano.spampinato@outlook.it>
2023-04-12 15:09:31 +0200

    project complete

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+:<template>
+  <div class="jumbo">
+    <AppTitle />
+    <AppHero />
+
+  </div>
+</template>
+
+<script>
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
+
+export default {
+  components: {
+    AppTitle,
+    AppHero
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+:<template>
+    <div>
+        <h3>{{ paragrafo }}</h3>
+        <button>Fammi Dormire</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'AppHero',
+    data () {
+        
+
+        return {
+            paragrafo: 'boolean non mi fa piu dormire la notte...premi il bottone per spegnermi!'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
